274ca2f9f1bf454c4964c7ff7f8cd3b7d7c90cfe
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2016-10-14 00:10:32 +0200

    add bugfix for openpgpjs which makes own keyserver obsolete

--- access_control.js
+++ access_control.js
@@ -35,6 +35,61 @@ Asymcrypt.saveInitiator = function (publicKey) {
 	});
 };
 
+
+// FIXME: Bugfix for openpgp.js
+// see: https://github.com/openpgpjs/openpgpjs/issues/477
+/**
+ * Search for a public key on the key server either by key ID or part of the user ID.
+ * @param  {String}   options.query   This can be any part of the key user ID such as name
+ *   or email address.
+ * @return {Array} An array of key IDs, fingerprints, usernames, mail addresses and key IDs
+ */
+openpgp.HKP.prototype.search = function (options) {
+  var uri = this._baseUrl + '/pks/lookup?op=index&options=mr&search=',
+      fetch = this._fetch;
+
+  if (options.query) {
+    uri += encodeURIComponent(options.query);
+  } else {
+    throw new Error('You must provide a query parameter!');
+  }
+
+  return fetch(uri).then(function (response) {
+    var result;
+    if (response.status === 404) {
+      return "";
+    } else if (response.status === 200) {
+      result = response.text();
+      return result;
+    }
+  }).then(function(uids){
+    var result, uidary, values;
+    uidary = uids.match(/pub:(.|\nuid)*/gm);
+    result = {};
+    for (var i in uidary) {
+      var keyID;
+      values = uidary[i].match(/pub:.*/)[0].split(":");
+      keyID = values[1].substr(24,16);
+      result[keyID] = {};
+      result[keyID].fingerprint = values[1];
+      result[keyID].algo = values[2];
+      result[keyID].keylength = values[3];
+      result[keyID].created = new Date(parseInt(values[4])*1000);
+      result[keyID].valid = new Date(parseInt(values[5])*1000);
+
+      result[keyID].uids = [];
+      values = uidary[i].match(/uid:.*/g);
+      for (var n in values) {
+        result[keyID].uids.push(values[n].split(":")[1]);
+      }
+    };
+    return result;
+  });
+};
+// End of Bugfix
+
+
+
 $(document).ready(function () {
 	/**
 	* if access_control.cgi is in the view where the admin password is set, asynchronous encryption will be an option
@@ -71,62 +126,44 @@ $(document).ready(function () {
 				$('#ac_admin').submit(function () {
 					if ($('#ac_admin #password0').length === 1 && $('#asymcrypt:checked').length === 1) {
 						if ($('#publicKey').length === 0 || $('#publicKey').val() === "") {
-							$.ajax({
-								url: Asymcrypt.extDir + '/keyserver.cgi',
-								data: {
-									service: 'peopleSearch',
-									name: $('#asymID').val()
-								},
-								method: "get",
-								timeout: (10 * 1000),
-								error: function (r, strError) {
-									if ($('#error').length === 0) {
-										var error = _("The name could not be found or the request was to inexplicit.");
-										$('<p id="error" class="shorttextcolumn"><span class="error">' + error + '</span></p>').insertBefore('#ac_admin :submit');
-									}
-								},
-								success: function (r) {
-									var possiblePublicKeys = JSON.parse(r), select, index;
-									if (possiblePublicKeys.length > 0) {
-										if ($('#publicKey').length === 0) {
-											select = '<tr><td></td><td><select name="publicKey" id="publicKey" size="1" style="width:300px">';
-											for (index in possiblePublicKeys) {
-												if (possiblePublicKeys[index] !== null) {
-													select += '<option>' + possiblePublicKeys[index] + '</option>';
-												}
-											}
-											select += '</select></td></tr>';
-											$(select).insertBefore($('#ac_admin tr:last'));
-											$('#ac_admin :submit').val(_("Save"));
-											$('#error').remove();
-										} else {
-											select = '<select name="publicKey" id="publicKey" size="1" style="width:300px">';
-											for (index in possiblePublicKeys) {
-												if (possiblePublicKeys[index] !== null) {
-													select += '<option>' + possiblePublicKeys[index] + '</option>';
-												}
-											}
-											select += '</select>';
-											$('#publicKey').replaceWith(select);
+							var hkp = new openpgp.HKP('https://pgp.mit.edu');
+							hkp.search({query: $('#asymID').val()}).then(function (people) {
+								var select, index, tr;
+								if (Object.keys(people).length > 0) {
+									select = $("<select />",{
+										name: "publicKey",
+										id: "publicKey",
+										size:"1",
+										style:"width:300px"
+									});
+									for (index in people) {
+										if (people[index] !== null) {
+											select.append($("<option />",{text : index + " " + people[index].uids[0]}));
 										}
 									}
+
+									if ($('#publicKey').length === 0) {
+										tr = $('<tr />').append($('<td/>')).append(select);
+										$(tr).insertBefore($('#ac_admin tr:last'));
+										$('#ac_admin :submit').val(_("Save"));
+										$('#error').remove();
+									} else {
+										$('#publicKey').replaceWith(select);
+									}
+								} else {
+									var error = _("The name could not be found or the request was to inexplicit.");
+									$('<p id="error" class="shorttextcolumn"><span class="error">' + error + '</span></p>').insertBefore('#ac_admin :submit');
 								}
 							});
 							return false;
 						} else {
-							var person = $('#publicKey').val().split(" ");
-							$.ajax({
-								url: Asymcrypt.extDir + '/keyserver.cgi',
-								data: {
-									service: 'getPublicKey',
-									keyid: person.shift()
-								},
-								method: "get",
-								success: function (pubKey) {
+							var keyID = $('#publicKey').val().split(" ")[0];
+							var hkp = new openpgp.HKP('https://pgp.mit.edu');
+							hkp.lookup({keyId: keyID}).then(function (pubKey) {
+								if (pubKey) {
 									Asymcrypt.saveInitiator(pubKey);
-								},
-								error: function (e) {
-									alert(e.responseText);
+								} else {
+									alert("No key for 0x"+keyID+"found!");
 								}
 							});
 						}
